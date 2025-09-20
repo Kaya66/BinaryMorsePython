27a728c7adaba6d89bd91dc004185861a3ec2ab3
Kaya66 <30756986+Kaya66@users.noreply.github.com>
2025-09-20 13:27:32 -0400

    Update BinaryMorseEncodeDecode.v1.0.py
    Remove excess comments

--- BinaryMorseEncodeDecode.v1.0.py
+++ BinaryMorseEncodeDecode.v1.0.py
@@ -1,16 +1,9 @@
-######################################################
-# Binary Morse Encode/Decode                         #
-# Author: Kaya                                       #
-# Version 1.0                                        #
-# Licenses GPLv2                                     #
-######################################################
 def main():
     #alphabet defines what characters are addrssable
     #binary is the morse/binary conversion of the alphabet.
-    #it is important that any edits to either one be adjusted in the other. the order is important to proper addressing of characters to their binary counterpart and vice versa.
     alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"," "]
     binary = ["10000010","01110100","01010100","01100011","10000001","11010100","00100011","11110100","11000010","10000100","01000011","10110100","00000010","01000010","00000011","10010100","00100100","10100011","11100011","00000001","11000011","11100100","10000011","01100100","01000100","00110100","00000101","10000101","11000101","11100101","11110101","11111101","01111101","00111101","00011101","00001101","00000111"]
-######################################################
+    
     def encode(alpha,bin,text):
         enc_result = []
         length = len(text)
@@ -25,11 +18,7 @@ def main():
                 else:
                     pass
         encode_format(enc_result)
-        #the encoder takes the alphabet and binary arrays for comparison to the user supplied text.
-        #the user supplied text then has each character compared to the alphabet.
-        #when a match is found. take the posiiton of the match form alphabet and get the binary segment form that same position and adds it to the encode_result.
-        #then its sent to the formater.
-######################################################
+
     def decode(alpha, bin, enc_text):
         dec_result = []
         for x in range (0, int(len(enc_text))):
@@ -41,18 +30,15 @@ def main():
                     dec_result.append(alpha[y])
                     break
         decode_format(dec_result)
-        #does the same thing as encode but in reverse. compare the binary block from enc_text to binary.  when a match is found, add the alphabet positoin to dec_result
-######################################################
+       
     def encode_format(encoded_text):
         finalstring = ' '.join(encoded_text)
         print(finalstring)
-        #remove strange formatting from arrays in python.  prints out just a clean line of binary separated by spaces.
-######################################################
+       
     def decode_format(decoded_text):
         finalstring = ''.join(decoded_text)
         print(finalstring)
-        #removes strange formatting from arrays in python. prints out a clean line of text.
-######################################################
+       
     def start():
         selection = input("Please select (E)ncode, or (D)ecode: ")
         non_array_text = input("Input your text to encode/decode: ")
